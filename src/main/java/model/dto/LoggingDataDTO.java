4b56d2888d2fe1c7015687de31c383cf46e9f83e
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2025-11-13 23:42:45 +0530

    Add Logging data DTO

---
+++ src/main/java/model/dto/LoggingDataDTO.java
@@ -0,0 +1,13 @@
+package model.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoggingDataDTO {
+    private String name;
+    private String password;
+}
